8df71d19196c2b46d9c6eebc4aaeb74012054bdb
JKot2020 <112009694+JKot2020@users.noreply.github.com>
2024-03-05 03:39:50 +0000

    Move header to App.js

--- StatMeal.js
+++
@@ -1,6 +0,0 @@
-// StatMeal.js
-// Author: Jason Kotowski
-// Email: 801jkotowski@gmail.com
-// Date: 3/4/2024
-//
-// Main codespace for StatMeal application
--- todo-reactnpx/src/App.js
+++ todo-reactnpx/src/App.js
@@ -1,3 +1,10 @@
+// StatMeal.js
+// Author: Jason Kotowski
+// Email: 801jkotowski@gmail.com
+// Date: 3/4/2024
+//
+// Main codespace for StatMeal application
+
 import logo from './logo.svg';
 import './App.css';
 
