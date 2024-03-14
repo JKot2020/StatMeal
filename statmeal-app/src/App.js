13e9e7e3af8a7fff4b6b6c61298532d692e6b836
JKot2020 <112009694+JKot2020@users.noreply.github.com>
2024-03-14 16:50:19 +0000

    Update file upload text

--- statmeal-app/src/App.js
+++ statmeal-app/src/App.js
@@ -80,8 +80,8 @@ class App extends Component {
   render() {
       return (
           <div>
-              <h1>GeeksforGeeks</h1>
-              <h3>File Upload using React!</h3>
+              <h1>StatMeal</h1>
+              <h3>Upload a spreadsheet file of your sales data</h3>
               <div>
                   <input
                       type="file"
