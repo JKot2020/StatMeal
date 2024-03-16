c1134b64af3c286ef3b0edc4e19d63652fddf770
JKot2020 <112009694+JKot2020@users.noreply.github.com>
2024-03-16 02:19:35 +0000

    Add get and load functions for .csv file

--- readFile.py
+++ readFile.py
@@ -6,3 +6,15 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import scipy as sp
 import seaborn as sns
+
+def get_file(file_name):
+    # Temp line for testing purposes
+    file_name = "Balaji Fast Food Sales.csv"
+    load_data(file_name)
+
+def load_data(file_name):
+    # read from .csv file
+    survey_data = pd.read_csv(file_name, keep_default_na=False)
+    
+    # create dataframe
+    data_combined = pd.DataFrame(survey_data)
