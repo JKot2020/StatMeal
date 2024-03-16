3fb8294fbe3ce5d5b031d16f5e7483259b5bbc3b
JKot2020 <112009694+JKot2020@users.noreply.github.com>
2024-03-16 03:02:08 +0000

    Add readFile import

--- server.py
+++ server.py
@@ -4,6 +4,7 @@
 from distutils.log import debug 
 from fileinput import filename 
 from flask import *
+from readFile import get_file
 
 app = Flask(__name__, template_folder='templates')
 
@@ -15,8 +16,8 @@ def main():
 def success():   
     if request.method == 'POST':   
         f = request.files['file'] 
-        f.save(f.filename)   
-        return render_template("Acknowledgement.html", name = f.filename)   
+        f.save(f.filename)
+        return render_template("Acknowledgement.html",name = f.filename)
   
 if __name__ == '__main__':   
     app.run(debug=True)
