333afdb3f8747c643533a3b6d696f946c8662a22
JKot2020 <112009694+JKot2020@users.noreply.github.com>
2024-03-16 16:02:25 +0000

    Convert list into dictionary

--- readFile.py
+++ readFile.py
@@ -7,15 +7,14 @@ import matplotlib.pyplot as plt
 import scipy as sp
 import seaborn as sns
 
-def get_file(file_name):
-    # Temp line for testing purposes
-    file_name = "Balaji Fast Food Sales.csv"
+def get_file(file):
+    file_name = file
     return load_data(file_name)
 
 def load_data(file_name):
     # read from .csv file
     receipt_data = pd.read_csv(file_name, keep_default_na=False)
 
-    column_names = list(receipt_data.columns)
+    column_names = {"columns": receipt_data.columns}
 
-    return column_names
+    return column_names["columns"]
