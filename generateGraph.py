081d2471b396cd1ec6544d62020e9a5dfd1f6d29
JKot2020 <112009694+JKot2020@users.noreply.github.com>
2024-03-17 17:36:39 +0000

    Add packages for graph generation

--- generateGraph.py
+++ generateGraph.py
@@ -1,2 +1,8 @@
 # generateGraph.py
 # Generates graph based on specified button prompt
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import scipy as sp
+import seaborn as sns
