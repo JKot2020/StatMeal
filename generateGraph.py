693a14aba147ac8aa2b30de3ea8a6d44c9b7389e
JKot2020 <112009694+JKot2020@users.noreply.github.com>
2024-03-17 18:12:31 +0000

    Add make_hist()

--- generateGraph.py
+++ generateGraph.py
@@ -6,3 +6,14 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import scipy as sp
 import seaborn as sns
+
+# histogram
+def make_hist():
+    file_name = "Balaji Fast Food Sales.csv"
+    receipt_data = pd.read_csv(file_name, keep_default_na=False)
+    
+    my_hist = sns.histplot(data=receipt_data['item_name']).set(title='item_name')
+    plt.xlabel('item_name')
+    plt.ylabel('Frequency')
+
+    return my_hist
