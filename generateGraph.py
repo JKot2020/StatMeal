b8708c3ebeed70eed4ec96979eddd07ed2997f39
JKot2020 <112009694+JKot2020@users.noreply.github.com>
2024-03-18 03:16:17 +0000

    Update make_hist()

--- generateGraph.py
+++ generateGraph.py
@@ -12,7 +12,8 @@ def make_hist():
     file_name = "Balaji Fast Food Sales.csv"
     receipt_data = pd.read_csv(file_name, keep_default_na=False)
     
-    my_hist = sns.histplot(data=receipt_data['item_name']).set(title='item_name')
+    my_hist = sns.displot(data=receipt_data['item_name']).set(title='item_name')
+    my_hist.set_xticklabels(rotation=45, horizontalalignment='right', fontweight='light', fontsize='large')
     plt.xlabel('item_name')
     plt.ylabel('Frequency')
 
