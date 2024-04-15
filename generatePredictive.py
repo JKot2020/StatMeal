4cc4b4ff89dce7e14621b116904a7cfdd88390bc
JKot2020 <112009694+JKot2020@users.noreply.github.com>
2024-04-15 00:54:56 +0000

    Add generatePredictive.py

---
+++ generatePredictive.py
@@ -0,0 +1,10 @@
+# generatePredictive.py
+# Generates predictive model based on specified button prompt
+
+import os
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import scipy as sp
+import seaborn as sns
+from pylab import *
