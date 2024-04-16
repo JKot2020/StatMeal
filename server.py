23169cc3dabc9c20da5f2fd7a1da16738e1a07bd
JKot2020 <112009694+JKot2020@users.noreply.github.com>
2024-04-16 02:23:13 +0000

    Add route to regressionOutput.html

--- server.py
+++ server.py
@@ -6,6 +6,7 @@ from fileinput import filename
 from flask import *
 from readFile import *
 from generateGraph import *
+from generatePredictive import *
 
 app = Flask(__name__, template_folder='templates')
 
@@ -18,18 +19,11 @@ def home():
     return render_template('statmeal.html')
   
 @app.route('/graph-maker', methods = ['POST'])   
-def predict():
+def graph():
     if request.method == 'POST':   
         f = request.files['file'] 
         f.save(f.filename)
         return render_template("graphMaker.html", name = f.filename, column = get_file(f.filename))
-
-@app.route('/predict-maker', methods = ['POST'])   
-def graph():   
-    if request.method == 'POST':   
-        f = request.files['file'] 
-        f.save(f.filename)
-        return render_template("predictMaker.html", name = f.filename, column = get_file(f.filename))
 
 @app.route('/graph-output', methods = ['POST'])
 def graph_output():
@@ -38,6 +32,20 @@ def graph_output():
         columns = request.form.get('columns')
         graph = request.form.get('graph')
         return render_template('graphMakerOutput.html', output_graph = sort_graph_data(file, columns, graph), test_graph = graph)
+    
+@app.route('/predict-maker', methods = ['POST'])   
+def predict():   
+    if request.method == 'POST':   
+        f = request.files['file'] 
+        f.save(f.filename)
+        return render_template("predictMaker.html", name = f.filename, column = get_file(f.filename))
+    
+@app.route('/regression-output', methods = ['POST'])
+def regression_output():
+    if request.method == 'POST':
+        file = request.form.get('file')
+        columns = request.form.get('columns')
+        return render_template('regressionOutput.html', output_regression = make_regression(file, columns))
 
 if __name__ == '__main__':   
     app.run(debug=True)
