51d08565b08bbee2522a5d118cbc4450c02ee300
JKot2020 <112009694+JKot2020@users.noreply.github.com>
2024-04-16 02:07:15 +0000

    Add make_regression()

--- generatePredictive.py
+++ generatePredictive.py
@@ -9,3 +9,15 @@ import scipy as sp
 import seaborn as sns
 import statsmodels.formula.api as smf
 from pylab import *
+
+# Generate regression model and return to web page
+def make_regression(file_name, column_names):
+    column_names = column_names[:-2]
+    column_names = column_names.split(", ")
+
+    receipt_data = pd.read_csv(open(file_name, encoding="utf-8", errors='ignore'), keep_default_na=False)
+    mod = smf.ols(formula = "column_names[0] ~ column_names[1]", data = receipt_data.dropna())
+    res = mod.fit()
+    my_summary = res.summary()
+    
+    return my_summary
