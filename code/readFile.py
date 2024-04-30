db870ac9fb89ceb803cc9fe178cd7c30087915db
JKot2020 <112009694+JKot2020@users.noreply.github.com>
2024-04-30 19:45:06 +0000

    Display data head

--- code/readFile.py
+++ code/readFile.py
@@ -1,11 +1,13 @@
 # readFile.py
 # Reads .csv file
 
+import os
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import scipy as sp
 import seaborn as sns
+from IPython.display import HTML
 
 def get_file(file):
     file_name = file
@@ -15,6 +17,10 @@ def load_data(file_name):
     # read from .csv file
     receipt_data = pd.read_csv(open(file_name, encoding="utf-8", errors='ignore'), keep_default_na=False)
 
+    my_path = os.path.abspath(__file__)
+    # Remove "/readFile.py" from file path
+    my_path = my_path[:-12]
+    receipt_data.head().to_html(my_path + "/static/head.html")
     column_names = {"columns": receipt_data.columns}
 
     return column_names["columns"]
