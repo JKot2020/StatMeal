66cc9c8a13a994bf7d2527b32be977edab0f4627
JKot2020 <112009694+JKot2020@users.noreply.github.com>
2024-05-01 12:54:34 +0000

    Remove unneeded libraries

--- code/generateGraph.py
+++ code/generateGraph.py
@@ -2,10 +2,8 @@
 # Generates graph based on specified button prompt
 
 import os
-import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import scipy as sp
 import seaborn as sns
 from pylab import *
 
